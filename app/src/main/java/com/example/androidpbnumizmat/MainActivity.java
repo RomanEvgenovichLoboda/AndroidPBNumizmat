5dd2501f82e82d998223814fb21c4e89127826a0
loboda.rom@gmail.com <loboda.rom@gmail.com>
2023-06-01 13:34:30 +0300

    Initial commit

---
+++ app/src/main/java/com/example/androidpbnumizmat/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.androidpbnumizmat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView1=findViewById(R.id.imageView1);
+    }
+
+    public void onClickImage(View view) {
+
+    }
+}
